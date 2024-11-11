09c6fe07a2819f51df455769a6aac0f9b870c20a
Nguyễn Bùi Nhật Hưng <shin11052017@gmail.com>
2024-11-12 05:59:20 +0700

    ket_noi_sql_trang_chu_va_hoan_thien_trang_chu

--- UI/trangchu/home.php
+++ UI/trangchu/home.php
@@ -16,7 +16,17 @@
     </style>
 </head>
 <body>
+<?php
+    require "config.php";
+?>
 
+<?php
+    $query = "SELECT * FROM `majors`";
+    $result = mysqli_query($conn,$query);
+    $query1 = "SELECT * FROM `users`";
+    $result1 = mysqli_query($conn,$query1);
+    $sltv=mysqli_num_rows($result1);
+?>
 <div class="container mt-5">
     <div class="header-section d-flex justify-content-between align-items-center mb-4">
         <form class="form-inline">
@@ -39,37 +49,29 @@
 
     <div class="row">
         <div class="col-lg-8">
-            <h2>Khoa CNTT</h2>
-            <div class="list-group mb-4">
-                <a href="#" class="list-group-item list-group-item-action">Mục 1: Hỏi Đáp về Công Nghệ</a>
-                <a href="#" class="list-group-item list-group-item-action">Mục 2: Hỏi Đáp về Cuộc Sống</a>
-                <a href="#" class="list-group-item list-group-item-action">Mục 3: Hỏi Đáp về Du Lịch</a>
-                <a href="#" class="list-group-item list-group-item-action">Mục 4: Hỏi Đáp về Sức Khỏe</a>
-                <a href="#" class="list-group-item list-group-item-action">Mục 5: Hỏi Đáp về Học Tập</a>
-            </div>
-            <h2>Khoa CNTT</h2>
-            <div class="list-group mb-4">
-                <a href="#" class="list-group-item list-group-item-action">Mục 1: Hỏi Đáp về Công Nghệ</a>
-                <a href="#" class="list-group-item list-group-item-action">Mục 2: Hỏi Đáp về Cuộc Sống</a>
-                <a href="#" class="list-group-item list-group-item-action">Mục 3: Hỏi Đáp về Du Lịch</a>
-                <a href="#" class="list-group-item list-group-item-action">Mục 4: Hỏi Đáp về Sức Khỏe</a>
-                <a href="#" class="list-group-item list-group-item-action">Mục 5: Hỏi Đáp về Học Tập</a>
-            </div>
-            <h2>Khoa CNTT</h2>
-            <div class="list-group mb-4">
-                <a href="#" class="list-group-item list-group-item-action">Mục 1: Hỏi Đáp về Công Nghệ</a>
-                <a href="#" class="list-group-item list-group-item-action">Mục 2: Hỏi Đáp về Cuộc Sống</a>
-                <a href="#" class="list-group-item list-group-item-action">Mục 3: Hỏi Đáp về Du Lịch</a>
-                <a href="#" class="list-group-item list-group-item-action">Mục 4: Hỏi Đáp về Sức Khỏe</a>
-                <a href="#" class="list-group-item list-group-item-action">Mục 5: Hỏi Đáp về Học Tập</a>
-            </div>
+        <?php
+            while ($row = mysqli_fetch_array($result))
+            {
+                echo "<h2>";
+                echo $row["major_info"];
+                echo "</h2>";
+                echo "
+                    <div class=\"list-group mb-4\">
+                    <a href=\"#\" class=\"list-group-item list-group-item-action\">Mục 1: Hỏi Đáp về Công Nghệ</a>
+                    <a href=\"#\" class=\"list-group-item list-group-item-action\">Mục 2: Hỏi Đáp về Cuộc Sống</a>
+                    <a href=\"#\" class=\"list-group-item list-group-item-action\">Mục 3: Hỏi Đáp về Du Lịch</a>
+                    <a href=\"#\" class=\"list-group-item list-group-item-action\">Mục 4: Hỏi Đáp về Sức Khỏe</a>
+                    <a href=\"#\" class=\"list-group-item list-group-item-action\">Mục 5: Hỏi Đáp về Học Tập</a>
+                    </div>
+                ";
+            }
+        ?>
         </div>
 
         <div class="col-lg-4">
             <div class="sidebar">
                 <h3>Tổng Số Người Sử Dụng</h3>
-                <p>500 Thành viên</p>
-
+                <?php echo "<p>".$sltv." Thành viên </p>" ?>
                 <h3>Bình Luận Mới Nhất</h3>
                 <ul class="list-unstyled">
                     <li>Người dùng 1: Câu hỏi về công nghệ.</li>
