b9f8d6ab90969c2780ed0d9e4ec28e2415cca354
Nguyễn Bùi Nhật Hưng <shin11052017@gmail.com>
2024-11-14 13:56:24 +0700

    tạo file footer, thực hiện làm phương thức tìm kiếm, link với trang chủ, link các giá trị sau khi tìm kiếm với thread, post đã có

---
+++ UI/trangchu/footer.php
@@ -0,0 +1,6 @@
+<footer class="footer">
+    <div>
+        © 2024 Copyright:
+        <a class="text-white" href="#">NTUCHAN</a>
+    </div>
+</footer>
--- UI/trangchu/home.php
+++ UI/trangchu/home.php
@@ -63,10 +63,7 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
-            <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
+            <?php require "../search/tool.php" ?>
             <div>
                 <?php if (!empty($username)): ?>
                     <a href="../nguoidung/user.php" class="mr-3 text-decoration-none">
