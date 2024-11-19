7c34a4c62ba8140cfb6faaa203411d5b41058a3b
Nguyễn Hoàng Minh <mingu03@yandex.com>
2024-11-19 09:33:01 +0700

    fixed logout not working

--- UI/trangchu/home.php
+++ UI/trangchu/home.php
@@ -56,27 +56,7 @@
         session_unset();
     }
     ?>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-3">
-        <a class="navbar-brand" href="../trangchu/home.php">NTUCHAN</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarSupportedContent">
-            <?php require "../search/tool.php" ?>
-            <div>
-                <?php if (!empty($username)): ?>
-                    <a href="../nguoidung/user.php" class="mr-3 text-decoration-none">
-                        <img src="<?php echo $profileImage; ?>" class="rounded-circle" width="40" height="40">
-                        <span class="font-weight-bold text-white"><?php echo htmlspecialchars($username); ?></span>
-                    </a>
-                <?php else: ?>
-                    <a href="../dangnhap/login.php"><button class="btn btn-primary">Login</button></a>
-                <?php endif; ?>
-                <a href="?logout" class="btn btn-secondary"><i class="fas fa-sign-out-alt"></i> Đăng Xuất</a>
-            </div>
-        </div>
-    </nav>
+    <?php session_abort(); require "./header.php" ?>
     <div class="container mt-5">
         <div class="row">
             <div class="col-lg-8">
