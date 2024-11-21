f37090a9ccd01d44540e6e21fd9951a1de7f0c62
Nguyễn Hoàng Minh <mingu03@yandex.com>
2024-11-21 20:07:10 +0700

    added weather info, format document

--- UI/trangchu/home.php
+++ UI/trangchu/home.php
@@ -56,7 +56,8 @@
         session_unset();
     }
     ?>
-    <?php session_abort(); require "./header.php" ?>
+    <?php session_abort();
+    require "./header.php" ?>
     <div class="container mt-5">
         <div class="row">
             <div class="col-lg-8">
@@ -69,7 +70,7 @@
                     mysqli_data_seek($result1, 0);
                     while ($row1 = mysqli_fetch_array($result1)) {
                         if ($row["board_id"] == $row1["board_id"]) {
-                            echo "<a href='../category/list.php?name=" . urlencode($row1["name"]) ."&category_id=" . urlencode($row1["category_id"]). "&sort=thread" ."' class=\"list-group-item list-group-item-action\">";
+                            echo "<a href='../category/list.php?name=" . urlencode($row1["name"]) . "&category_id=" . urlencode($row1["category_id"]) . "&sort=thread" . "' class=\"list-group-item list-group-item-action\">";
                             echo $row1["name"];
                             echo "</a>";
                         }
@@ -89,11 +90,21 @@
                         <?php
                         while ($row = mysqli_fetch_array($result3)) {
                             echo "<li>";
-                            echo '<a href="../threads/thread.php?id='.urlencode($row["thread_id"]).'">'.$row["title"]."</a>";
+                            echo '<a href="../threads/thread.php?id=' . urlencode($row["thread_id"]) . '">' . $row["title"] . "</a>";
                             echo "</li>";
                         }
                         ?>
                     </ul>
+                </div>
+                <div>
+                    <!--Begin thoitiet.vn Weather Widget -->
+                    <iframe
+                        src="https://thoitiet.vn/embed/wvrvhpduxgk?days=3&hC=%23ffffff&hB=%23FF0000&tC=%23848484&bC=%23FF0000&lC=%23dddddd"
+                        id="widgeturl" width="100%" height="297" scrolling="no" frameborder="0" allowtransparency="true"
+                        style="border:none;overflow:hidden;">
+
+                    </iframe>
+                    <!-- End Widget -->
                 </div>
             </div>
         </div>
