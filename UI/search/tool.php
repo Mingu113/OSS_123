3c07445a1b09276391d8e9ebc7f50141897a129e
Zinxus <hieu123lio71@gmail.com>
2024-11-22 00:30:28 +0700

    sửa cấu trúc folders

--- UI/search/tool.php
+++
@@ -1,4 +0,0 @@
-<form class="form-inline" action="../search/result.php" method="post">
-    <input class="form-control mr-sm-2" type="search" name="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-</form>
