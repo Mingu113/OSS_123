d5b8ddc26ecaf6202d51a134414b31d668c6cd13
Nguyễn Hoàng Minh <mingu03@yandex.com>
2024-11-22 02:02:09 +0700

    update database

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     $hostname = "localhost:3306";
     $username = "ntu304@localhost";
     $password = "";
-    $db = "ntuchan";
+    $db = "ntu304_ntuchan";
     $conn = mysqli_connect($hostname,$username,$password,$db);
     if(!$conn) {
         die("Error: ". mysqli_connect_error());
