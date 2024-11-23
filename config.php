c769ff3beb0bd345ed3dd7cccf4fad3143bb98a6
Bảo Trương <baodeptrai0707@gmail.com>
2024-11-23 15:57:03 +0700

    admin_index (them board + category), fix layout

--- config.php
+++ config.php
@@ -1,8 +1,19 @@
 <?php 
-    $hostname = "localhost:3306";
-    $username = "ntu304@localhost";
+    // $hostname = "localhost:3306";
+    // $username = "ntu304@localhost";
+    // $password = "";
+    // $db = "ntu304_ntuchan";
+    // $conn = mysqli_connect($hostname,$username,$password,$db);
+    // if(!$conn) {
+    //     die("Error: ". mysqli_connect_error());
+    //}
+?>
+
+<?php 
+    $hostname = "localhost";
+    $username = "root";
     $password = "";
-    $db = "ntu304_ntuchan";
+    $db = "ntuchan";
     $conn = mysqli_connect($hostname,$username,$password,$db);
     if(!$conn) {
         die("Error: ". mysqli_connect_error());
